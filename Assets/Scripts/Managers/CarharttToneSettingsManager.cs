7389ca13a648b47f5190f8c5806c9b750d4ed268
Luciano Donati <me@lucianodonati.com>
2018-10-21 21:00:56 -0300

    Added CarharttMode.unity scene. Modified scripts, started carhartt coding.

---
+++ Assets/Scripts/Managers/CarharttToneSettingsManager.cs
@@ -0,0 +1,103 @@
+﻿using Tones.Managers;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CarharttToneSettingsManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text freqText = null;
+    [SerializeField]
+    private Button freqDown = null, freqUp = null;
+
+    [SerializeField]
+    private Text dBText = null;
+    [SerializeField]
+    private Button dBDown = null, dBUp = null;
+    private const int dbMin = 0, dbMax = 100;
+    private const int dbDelta = 5;
+    private int freqIndex = 0, currentDB = 20;
+
+    private void Start()
+    {
+        currentDB = 20;
+        freqIndex = 2; // Para 500
+    }
+
+    public void IncreaseFrequency()
+    {
+        freqIndex++;
+
+        if (5 == freqIndex)
+        {
+            freqUp.interactable = false;
+        }
+
+        if (!freqDown.interactable)
+        {
+            freqDown.interactable = true;
+        }
+
+        UpdateFrequencyUI();
+    }
+
+    public void DecreaseFrequency()
+    {
+        freqIndex--;
+
+        if (2 == freqIndex)
+        {
+            freqDown.interactable = false;
+        }
+
+        if (!freqUp.interactable)
+        {
+            freqUp.interactable = true;
+        }
+
+        UpdateFrequencyUI();
+    }
+
+    private void UpdateFrequencyUI()
+    {
+        freqText.text = TestManager.frequencies[freqIndex] + "\nHz";
+    }
+
+    public void IncreaseVolume()
+    {
+        currentDB += dbDelta;
+
+        if (currentDB == dbMax)
+        {
+            dBUp.interactable = false;
+        }
+
+        if (!dBDown.interactable)
+        {
+            dBDown.interactable = true;
+        }
+
+        UpdateDBUI();
+    }
+
+    public void DecreaseVolume()
+    {
+        currentDB -= dbDelta;
+
+        if (currentDB == dbMin)
+        {
+            dBDown.interactable = false;
+        }
+
+        if (!dBUp.interactable)
+        {
+            dBUp.interactable = true;
+        }
+
+        UpdateDBUI();
+    }
+
+    private void UpdateDBUI()
+    {
+        dBText.text = currentDB + "\ndB";
+    }
+}
---
+++ Assets/Scripts/Managers/Tests/CarharttTestManager.cs
@@ -0,0 +1,115 @@
+﻿using Managers;
+using Tones.Sessions;
+using Tones.Tools;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Tones.Managers
+{
+    public class CarharttTestManager : TestManager
+    {
+        [SerializeField]
+        private PushButton pacientButton = null;
+        [SerializeField]
+        private PushButton manualSessionButton = null;
+
+        [SerializeField]
+        private Button[] interactableDuringSession = null;
+        private bool[] previousState = null;
+
+        [SerializeField]
+        private Text pacientName = null;
+
+        [SerializeField]
+        private Animator ledLight = null;
+
+
+        private void Start()
+        {
+            pacientName.text = DataManager.Instance.CurrentPacient.ToString();
+
+            manualSessionButton.onButtonDown.AddListener(ManualButtonDown);
+            manualSessionButton.onButtonUp.AddListener(ManualButtonUp);
+
+            previousState = new bool[interactableDuringSession.Length];
+        }
+
+        private void ManualButtonDown()
+        {
+            //manualSessionButton.onButtonDown.RemoveListener(ManualButtonDown);
+            if (!OngoingTest)
+            {
+                StartTest();
+            } (currentSession as Manual).StartTone();
+        }
+
+        private void ManualButtonUp()
+        {
+            (currentSession as Manual).StopTone();
+
+            SessionEnd();
+        }
+
+        public override void StartTest()
+        {
+            base.StartTest();
+            Debug.Log("Manual Classic test Started.");
+            currentSession = new Manual(currentFrequencyIndex, currentVolume, this, ear);
+
+            for (int i = 0; i < interactableDuringSession.Length; i++)
+            {
+                previousState[i] = interactableDuringSession[i].interactable;
+                interactableDuringSession[i].interactable = false;
+            }
+
+            pacientButton.onButtonDown.AddListener(currentSession.PacientButtonDown);
+            pacientButton.onButtonUp.AddListener(currentSession.PacientButtonUp);
+            pacientButton.onButtonDown.AddListener(LedOn);
+            pacientButton.onButtonUp.AddListener(LedOff);
+        }
+
+        public void SessionEnd()
+        {
+            pacientButton.onButtonDown.RemoveListener(currentSession.PacientButtonDown);
+            pacientButton.onButtonUp.RemoveListener(currentSession.PacientButtonUp);
+            pacientButton.onButtonDown.RemoveListener(LedOn);
+            pacientButton.onButtonUp.RemoveListener(LedOff);
+
+            for (int i = 0; i < interactableDuringSession.Length; i++)
+            {
+                interactableDuringSession[i].interactable = previousState[i];
+            }
+
+            currentSession.EndSession();
+        }
+
+        private void LedOn()
+        {
+            ledLight.SetTrigger("On");
+        }
+
+        private void LedOff()
+        {
+            ledLight.SetTrigger("Off");
+        }
+
+        public override void SessionEnd(bool sessionSucceded)
+        {
+            base.SessionEnd(sessionSucceded);
+
+            currentSession = null;
+        }
+
+        public void ShowGraph()
+        {
+            if (null != succesfulSessions)
+            {
+                // TODO: Connect with user and persist data
+                foreach (var session in succesfulSessions)
+                {
+                    GraphManager.Instance.GraphSession(session.Value);
+                }
+            }
+        }
+    }
+}
